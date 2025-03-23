d435e6d48e547a3a7aed19ffa22f132f9e6d03d5
Slengm4n <99735480+Slengm4n@users.noreply.github.com>
2025-03-22 22:31:50 -0300

    delete consertado

--- controllers/film_delete.php
+++ controllers/film_delete.php
@@ -4,8 +4,11 @@ require_once __DIR__ . "/../config/database.php";
 
 $id = $_GET['id'];
 
-$FilmDeleteQuery = "DELETE FROM film WHERE id=?";
-$stmt = $pdo->prepare($FilmDeleteQuery);
-$stmt->execute([$id]);
+$FilmDeleteQuery = "DELETE FROM film WHERE id= '$id' ";
 
-$stmt->close();
+if(mysqli_query($conn, $FilmDeleteQuery))
+{
+    header("Location: ../pages/stock.php?status=success");
+}else{
+    header("Location: ../pages/stock.php?status=error");
+}
